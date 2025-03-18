834370e5750c064d4c2922b6d6fababbfa84d4b9
Elie ABI CHAR <eliabichar@hotmail.com>
2025-03-18 12:17:58 +0100

    Initial Commit

---
+++ dev/devApp.swift
@@ -0,0 +1,17 @@
+//
+//  devApp.swift
+//  dev
+//
+//  Created by Elie Abi Char on 18/03/2025.
+//
+
+import SwiftUI
+
+@main
+struct devApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ devTests/devTests.swift
@@ -0,0 +1,16 @@
+//
+//  devTests.swift
+//  devTests
+//
+//  Created by Elie Abi Char on 18/03/2025.
+//
+
+import Testing
+
+struct devTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
